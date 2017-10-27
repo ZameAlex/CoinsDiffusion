83eb2bd59c6b14aad8083b217ed8ec4c9a773086
ZameAlex <zamekula.oleksii@gmail.com>
2017-10-27 22:28:47 +0300

    Fixed bug of invalid input

--- Coin_Diffusion/Program.cs
+++ Coin_Diffusion/Program.cs
@@ -52,52 +52,77 @@ namespace Coin_Diffusion
             {
                 List<City> cities = new List<City>();
                 Hashtable table = new Hashtable();
-                int c = Convert.ToInt32(reader.ReadLine());
-                if (c == 0)
-                    break;
-                Country[] countries = new Country[c];
-                
+                int c;
+                bool result = Int32.TryParse(reader.ReadLine(), out c);
+                if (result)
+                {
+                    if (c == 0)
+                        break;
+                    Country[] countries = new Country[c];
+
                     for (int i = 0; i < c; i++)
                     {
-                        int[] coords = new int[4];
-                        string[] buf = reader.ReadLine().Split(new char[] { ' ' });
-                        countries[i].Name = buf[0];
-                        countries[i].x1 = Convert.ToInt32(buf[1]);
-                        countries[i].y1 = Convert.ToInt32(buf[2]);
-                        countries[i].x2 = Convert.ToInt32(buf[3]);
-                        countries[i].y2 = Convert.ToInt32(buf[4]);
-                    }
-                if (c > 1)
-                {
-                    Array.Sort(countries);
-                    string[] names = new string[c];
-                    int h = 0;
-                    foreach (var item in countries)
-                    {
-                        names[h] = item.Name;
-                        h++;
-                        for (int k = item.x1; k <= item.x2; k++)
-                            for (int j = item.y1; j <= item.y2; j++)
+                        try
+                        {
+                            int[] coords = new int[4];
+                            string[] buf = reader.ReadLine().Split(new char[] { ' ' });
+                            countries[i].Name = buf[0];
+                            countries[i].x1 = Convert.ToInt32(buf[1]);
+                            countries[i].y1 = Convert.ToInt32(buf[2]);
+                            countries[i].x2 = Convert.ToInt32(buf[3]);
+                            countries[i].y2 = Convert.ToInt32(buf[4]);
+                            if (countries[i].x1 <= 0 || countries[i].y1 <= 0 ||
+                                countries[i].x2 <= 0 || countries[i].y2 <= 0)
                             {
-                                int xNeighbour = (k - 1) * 10 + j;
-                                int yNeighbour = k * 10 + j - 1;
-                                var city = new City(k, j, item.Name);
-                                cities.Add(city);
-                                table.Add(k * 10 + j, city);
-                                if (table.Contains(yNeighbour))
-                                    city.MakeLinkWithNeighbours(table[yNeighbour] as City);
-                                if (table.Contains(xNeighbour))
-                                    city.MakeLinkWithNeighbours(table[xNeighbour] as City);
+                                Console.WriteLine("Incorrect data, requires all coordinates biger then 0");
+                                goto incorrect_data;
                             }
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine("Incorrect data\n"+e.Message);
+                            goto incorrect_data;
+                        }
                     }
+                    if (c > 1)
+                    {
+                        Array.Sort(countries);
+                        string[] names = new string[c];
+                        int h = 0;
+                        foreach (var item in countries)
+                        {
+                            names[h] = item.Name;
+                            h++;
+                            for (int k = item.x1; k <= item.x2; k++)
+                                for (int j = item.y1; j <= item.y2; j++)
+                                {
+                                    int xNeighbour = (k - 1) * 10 + j;
+                                    int yNeighbour = k * 10 + j - 1;
+                                    var city = new City(k, j, item.Name);
+                                    cities.Add(city);
+                                    table.Add(k * 10 + j, city);
+                                    if (table.Contains(yNeighbour))
+                                        city.MakeLinkWithNeighbours(table[yNeighbour] as City);
+                                    if (table.Contains(xNeighbour))
+                                        city.MakeLinkWithNeighbours(table[xNeighbour] as City);
+                                }
+                        }
 
-                    Transition transition = new Transition(cities, names);
-                    Write(++count, transition.CountDuration());
+                        Transition transition = new Transition(cities, names);
+                        Write(++count, transition.CountDuration());
+                    }
+                    else
+                        Write(++count, new Dictionary<string, int>() { { countries[0].Name, 0 } });
                 }
                 else
-                    Write(++count, new Dictionary<string, int>() { { countries[0].Name, 0 } });
+                {
+                    Console.WriteLine("Incorrect data");
+                    break;
+                }
+            incorrect_data:;
             }
-            Console.Read();
+            
+            //Console.Read();
         }
     }
 }
